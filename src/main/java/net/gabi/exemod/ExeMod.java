8e5234c312fe9f41dd030585939f2cbd982504f0
Gabriel <gs.duke_dev@yahoo.com>
2026-09-10 00:02:09 -0300

    Initial commit

---
+++ src/main/java/net/gabi/exemod/ExeMod.java
@@ -0,0 +1,23 @@
+package net.gabi.exemod;
+
+import net.fabricmc.api.ModInitializer;
+
+import net.minecraft.util.Identifier;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class ExeMod implements ModInitializer {
+	public static final String MOD_ID = "exe-mod";
+
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+		LOGGER.info("Hello Fabric world!");
+	}
+
+	public static Identifier id(String path) {
+		return Identifier.of(MOD_ID, path);
+	}
+}
---
+++ src/main/java/net/gabi/exemod/ExeModClient.java
@@ -0,0 +1,10 @@
+package net.gabi.exemod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExeModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
