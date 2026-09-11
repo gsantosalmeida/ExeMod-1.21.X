1f1c38cb9af1879864814d01f56e52809a7a5ec6
Gabriel <gs.duke_dev@yahoo.com>
2026-09-11 11:51:27 -0300

    Adicionados mais itens, turmalinas e turmalinas brutas

--- src/main/java/net/gabi/exemod/item/ModItems.java
+++ src/main/java/net/gabi/exemod/item/ModItems.java
@@ -10,7 +10,13 @@ import net.minecraft.util.Identifier;
 
 public class ModItems{
     public static final Item TURMALINA_SCHORL = registerItem("turmalina_schorl", new Item(new Item.Settings()));
-
+    public static final Item TURMALINA_INDICOLITA = registerItem("turmalina_indicolita", new Item(new Item.Settings()));
+    public static final Item TURMALINA_RUBELITA = registerItem("turmalina_rubelita", new Item(new Item.Settings()));
+    public static final Item TURMALINA_PARAIBA = registerItem("turmalina_paraiba", new Item(new Item.Settings()));
+    public static final Item TURMALINA_SCHORL_BRUTA = registerItem("turmalina_schorl_bruta", new Item(new Item.Settings()));
+    public static final Item TURMALINA_INDICOLITA_BRUTA = registerItem("turmalina_indicolita_bruta", new Item(new Item.Settings()));
+    public static final Item TURMALINA_RUBELITA_BRUTA = registerItem("turmalina_rubelita_bruta", new Item(new Item.Settings()));
+    public static final Item TURMALINA_PARAIBA_BRUTA = registerItem("turmalina_paraiba_bruta", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(ExeMod.MOD_ID, name), item);
@@ -20,6 +26,13 @@ public class ModItems{
 
         ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS).register(entries -> {
             entries.add(TURMALINA_SCHORL);
+            entries.add(TURMALINA_INDICOLITA);
+            entries.add(TURMALINA_RUBELITA);
+            entries.add(TURMALINA_PARAIBA);
+            entries.add(TURMALINA_SCHORL_BRUTA);
+            entries.add(TURMALINA_INDICOLITA_BRUTA);
+            entries.add(TURMALINA_RUBELITA_BRUTA);
+            entries.add(TURMALINA_PARAIBA_BRUTA);
         });
     }
 }
