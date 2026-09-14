e520d94b916778f5c341b5f677b480b4c3f6182c
Gabriel <gs.duke_dev@yahoo.com>
2026-09-14 20:51:51 -0300

    Adicionados mais itens!

--- src/main/java/net/gabi/exemod/item/ModItems.java
+++ src/main/java/net/gabi/exemod/item/ModItems.java
@@ -17,6 +17,10 @@ public class ModItems{
     public static final Item TURMALINA_INDICOLITA_BRUTA = registerItem("turmalina_indicolita_bruta", new Item(new Item.Settings()));
     public static final Item TURMALINA_RUBELITA_BRUTA = registerItem("turmalina_rubelita_bruta", new Item(new Item.Settings()));
     public static final Item TURMALINA_PARAIBA_BRUTA = registerItem("turmalina_paraiba_bruta", new Item(new Item.Settings()));
+    public static final Item METADE_CARVAO = registerItem("metade_carvao", new Item(new Item.Settings()));
+    public static final Item BARRA_FERRO_CARVAO = registerItem("barra_ferro_carvao", new Item(new Item.Settings()));
+    public static final Item BARRA_ACO = registerItem("barra_aco", new Item(new Item.Settings()));
+    public static final Item CABO_ACO = registerItem("cabo_aco", new Item(new Item.Settings()));
 
     private static Item registerItem(String name, Item item){
         return Registry.register(Registries.ITEM, Identifier.of(ExeMod.MOD_ID, name), item);
@@ -33,6 +37,10 @@ public class ModItems{
             entries.add(TURMALINA_INDICOLITA_BRUTA);
             entries.add(TURMALINA_RUBELITA_BRUTA);
             entries.add(TURMALINA_PARAIBA_BRUTA);
+            entries.add(METADE_CARVAO);
+            entries.add(BARRA_FERRO_CARVAO);
+            entries.add(BARRA_ACO);
+            entries.add(CABO_ACO);
         });
     }
 }
