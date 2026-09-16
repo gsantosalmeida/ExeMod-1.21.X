9134821ad7b728885c143cdb6afb4d2687d74894
Gabriel <gs.duke_dev@yahoo.com>
2026-09-16 11:15:18 -0300

    Datagens Itens criados!

--- src/main/java/net/gabi/exemod/datagen/ModItemTagProvider.java
+++ src/main/java/net/gabi/exemod/datagen/ModItemTagProvider.java
@@ -1,4 +1,25 @@
 package net.gabi.exemod.datagen;
 
-public class ModItemTagProvider {
+import net.fabricmc.fabric.api.datagen.v1.FabricDataOutput;
+import net.fabricmc.fabric.api.datagen.v1.provider.FabricTagProvider;
+import net.gabi.exemod.block.ModBlocks;
+import net.gabi.exemod.item.ModItems;
+import net.gabi.exemod.util.ModTags;
+import net.minecraft.registry.RegistryWrapper;
+
+import java.util.concurrent.CompletableFuture;
+
+public class ModItemTagProvider extends FabricTagProvider.ItemTagProvider {
+    public ModItemTagProvider(FabricDataOutput output, CompletableFuture<RegistryWrapper.WrapperLookup> completableFuture) {
+        super(output, completableFuture);
+    }
+
+    @Override
+    protected void configure(RegistryWrapper.WrapperLookup lookup) {
+        getOrCreateTagBuilder(ModTags.Items.ITENS_TRANSFORMS)
+                .add(ModItems.TURMALINA_INDICOLITA)
+                .add(ModItems.TURMALINA_SCHORL)
+                .add(ModItems.TURMALINA_PARAIBA)
+                .add(ModItems.TURMALINA_RUBELITA);
+    }
 }
